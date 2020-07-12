82e7b2720a3e1367912107f4cbcfbd38f7305a46
hajime nakamura <h.nakamura0903@gmail.com>
2020-07-12 11:34:43 +0900

    ch03-025 Riot.jsにおけるスタイリングの仕方 - CSSスコープ

---
+++ sec025/src/main.js
@@ -0,0 +1,8 @@
+import '@riotjs/hot-reload'
+import App from './app.riot'
+import {component} from 'riot'
+
+component(App)(document.getElementById('app'), {
+  title: 'Hello Riot.js',
+  subTitle: 'This is parent'
+})
