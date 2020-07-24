78e829723b3de73f99e33223206689e51e86ce62
hajime nakamura <h.nakamura0903@gmail.com>
2020-07-24 11:32:52 +0900

    ch06-030 CMSのUIの実装 - メインコンテンツの実装

--- sec030/src/main.js
+++ sec030/src/main.js
@@ -1,9 +1,11 @@
 import '@riotjs/hot-reload'
-import {component} from 'riot'
+import {component, register} from 'riot'
+import {Router, Route} from '@riotjs/route'
 import 'semantic-ui-css/semantic.min.css'
 import 'semantic-ui-riot'
 import App from './app.riot'
 
-component(App)(document.getElementById('app'), {
-  title: 'CMS'
-})
+register('router', Router)
+register('route', Route)
+
+component(App)(document.getElementById('app'))
